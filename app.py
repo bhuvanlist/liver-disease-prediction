9e691b280645fcd38ae83525f8979825b5ed5246
bhuvanlist <bhuvanchandu26@gmail.com>
2025-09-18 07:24:50 +0530

    Update app.py
    Signed-off-by: bhuvanlist <bhuvanchandu26@gmail.com>

--- app.py
+++ app.py
@@ -99,9 +99,9 @@ if page == "Home":
 
     col1, col2 = st.columns(2)
     with col1:
-        st.image("static/images/liver1.jpg", caption="Human Liver Anatomy", use_container_width=True)
+        st.image("static/images/liver1.jpg", caption="Human Liver Anatomy")
     with col2:
-        st.image("static/images/liver2.jpg", caption="Liver Position in Human Body", use_container_width=True)
+        st.image("static/images/liver2.jpg", caption="Liver Position in Human Body")
 
 # -------------------------------
 # Prediction Page
